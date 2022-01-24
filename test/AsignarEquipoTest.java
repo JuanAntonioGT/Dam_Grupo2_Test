6c12ab543f633df4a7a18c5c9deba8233ba19639
Suimoitra <92159742+Suimoitra@users.noreply.github.com>
2022-01-24 22:38:51 +0100

    Modificado según nuevos criterios

--- test/AsignarEquipoTest.java
+++ test/AsignarEquipoTest.java
@@ -2,56 +2,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * 
- */
 
-/**
- * @author ArtsiomKrautsouKharl
- *
- */
-class AsignarEquipoTest {
-
-	
-	/**
-	 * Se prueba Set nombre, para ver que existe en BDD y no puede estar metido
-	 */
-	@Test
-	void testSetEquipoPruebaExistenciaBDD() {
-		Jugador equipoUno = new Jugador();
-	    Jugador equipoDos = new Jugador();
-	    
-		String e1nombre = "CasasCasasCasasCasasCasas";
-		String e2nombre = "CasasCasasCasasCasasCasas";
-		
-		equipoUno.setNombreJugador(e1nombre);
-		equipoDos.setNombreJugador(e2nombre);
-		
-		 assertEquals(null,equipoUno.getNombreJugador().equals(equipoDos.getNombreJugador()));
-		 assertEquals(null,equipoUno.getNombreJugador().equals(equipoDos.getNombreJugador()));
-		 assertEquals(null,equipoUno.getNombreJugador().equals(equipoDos.getNombreJugador()));
-		
-		/*fail("Not yet implemented");*/
-	}
-	
-	/**
-	 * Se prueba Set nombre, para ver que no existe en BDD
-	 */
-	@Test
-	void testSetEquipoPruebaNoExistenciaBDD() {
-		Equipo equipoUno = new Equipo();
-	    Equipo equipoDos = new Equipo();
-	    
-		String e1nombre = "CasasCasasCasasCasasCasas";
-		String e2nombre = "CasasCasasCasasCasas";
-		
-		equipoUno.setNombreEquipo(e1nombre);
-		equipoDos.setNombreEquipo(e2nombre);
-		
-		 assertEquals(,equipoUno.getNombreEquipo().equals(equipoDos.getNombreEquipo()));
-		
-		/*fail("Not yet implemented");*/
-	}
 	
 	/*========================================================*/
 	/*========================================================*/
@@ -61,85 +12,61 @@ class AsignarEquipoTest {
 	
 	
 	/**
-	 * Se prueba Set nombre, para ver que existe en BDD y que no se pueda introducir
+	 * Asignar jugador que exista y tenga todos los campos válidos
 	 */
-	@Test
-	void testSetNombreJugadorPruebaExistenciaBDD() {
-		Jugador jugadorUno = new Jugador();
-	    Jugador jugadorDos = new Jugador();
-	    
-		String j1nombre = "MariaMariaMariaMaria";
-		String j2nombre = "MariaMariaMariaMaria";
-		
-		int edadj1 = 31;
-	    int edadj2 = 31;
-	    
-	    String idiomaj1 = "Español";
-	    String idiomaj2 = "Español";
-	    
-	    String idioma1 = "Ingles";
-	    String idioma2 = "Frances";
-	    String idioma3 = "Aleman";
-	    String idioma4 = "Español";
-	    
-		jugadorUno.setNombreJugador(j1nombre);
-		jugadorUno.setEdad(edadj1);
-	    jugadorUno.setIdioma(idiomaj1);
-	    
-		jugadorDos.setNombreJugador(j2nombre);
-		jugadorDos.setEdad(edadj2);
-	    jugadorDos.setIdioma(idiomaj2);
-	    
-	   /* jugadorDos.setIdioma(jugadorDos.getIdioma()+idioma1);
-        jugadorDos.setIdioma(jugadorDos.getIdioma()+idioma2);
-        jugadorDos.setIdioma(jugadorDos.getIdioma()+idioma3);*/
-		
-		
-	    assertAll("existencia",
-	            () -> assertEquals(null,jugadorUno.getNombreJugador().equals(jugadorDos.getNombreJugador())),
-	            () -> assertEquals(null,jugadorDos.getEdad() == jugadorUno.getEdad()),
-	            () -> assertEquals(null, jugadorUno.getIdioma().equals(jugadorDos.getIdioma())));
-		
-		/*fail("Not yet implemented");*/
-	}
+    @Test
+    void testSetJugadorTodoCorrecto() {
+
+        AsignarEquipo asignarEquipo = new AsignarEquipo();
+
+        /*Creamos un jugador con todos los campos válidos*/
+        Jugador jugador = new Jugador();
+        String nombrevalido = "Maria";
+        int edadValida = 32;
+        String idiomaValido = "Español";
+        
+        jugador.setNombreJugador(nombrevalido);
+        jugador.setEdad(edadValida);
+        jugador.setIdioma(idiomaValido);
+
+        /*Asignar el jugador creado en asignar equipo*/
+        asignarEquipo.setJugador(jugador);
+
+        Jugador jugadorGuardado = asignarEquipo.getJugador();
+       
+        assertNotNull(jugadorGuardado);
+        assertEquals(nombrevalido, jugadorGuardado.getNombreJugador());
+        assertEquals(edadValida, jugadorGuardado.getEdad());
+        assertEquals(idiomaValido, jugadorGuardado.getIdioma());
+    }
+	
 	
 	/**
 	 * Se prueba Set nombre, para ver que no existe en BDD
 	 */
 	@Test
 	void testSetNombreJugadorPruebaNoExistenciaBDD() {
-		Jugador jugadorUno = new Jugador();
-	    Jugador jugadorDos = new Jugador();
-	    
-		String j1nombre = "MariaMariaMariaMaria";
-		String j2nombre = "MariaMariaMaria";
-		
-		int edadj1 = 31;
-	    int edadj2 = 31;
-	    
-	    String idiomaj1 = "Español";
-	    String idiomaj2 = "Español";
-	    
-	    String idioma1 = "Ingles";
-	    String idioma2 = "Frances";
-	    String idioma3 = "Aleman";
-	    String idioma4 = "Español";
-	    
-		jugadorUno.setNombreJugador(j1nombre);
-		jugadorUno.setEdad(edadj1);
-	    jugadorUno.setIdioma(idiomaj1);
-	    
-		jugadorDos.setNombreJugador(j2nombre);
-		jugadorUno.setEdad(edadj2);
-	    jugadorUno.setIdioma(idiomaj2);
-		
-		
-	    assertAll("existencia",
-	            () -> assertFalse(jugadorUno.getNombreJugador().equals(jugadorDos.getNombreJugador())),
-	            () -> assertEquals(edadj2,jugadorDos.getEdad()),
-	            () -> assertEquals(idiomaj2,jugadorDos.getIdioma()));
-		
-		/*fail("Not yet implemented");*/
+		AsignarEquipo asignarEquipo = new AsignarEquipo();
+
+        /*Creamos un jugador con todos los campos válidos*/
+        Jugador jugador = new Jugador();
+        String nombrevalido = "";
+        int edadValida = 0;
+        String idiomaValido = "";
+        
+        jugador.setNombreJugador(nombrevalido);
+        jugador.setEdad(edadValida);
+        jugador.setIdioma(idiomaValido);
+
+        /*Asignar el jugador creado en asignar equipo*/
+        asignarEquipo.setJugador(jugador);
+
+        Jugador jugadorGuardado = asignarEquipo.getJugador();
+       
+        assertNotNull(jugadorGuardado);
+        assertEquals(null, jugadorGuardado.getNombreJugador());
+        assertEquals(0, jugadorGuardado.getEdad());
+        assertEquals(null, jugadorGuardado.getIdioma());
 	}
 	
 	/**
@@ -147,38 +74,27 @@ class AsignarEquipoTest {
 	 */
 	@Test
 	void testSetNombreJugadorNombreIncorrecto() {
-		Jugador jugadorUno = new Jugador();
-	    Jugador jugadorDos = new Jugador();
-	    
-		String j1nombre = "MariaMariaMariaMaria";
-		String j2nombre = "1234";
-		
-		int edadj1 = 31;
-	    int edadj2 = 30;
-	    
-	    String idiomaj1 = "Español";
-	    String idiomaj2 = "Español";
-	    
-	    String idioma1 = "Ingles";
-	    String idioma2 = "Frances";
-	    String idioma3 = "Aleman";
-	    String idioma4 = "Español";
-	    
-		jugadorUno.setNombreJugador(j1nombre);
-		jugadorUno.setEdad(edadj1);
-	    jugadorUno.setIdioma(idiomaj1);
-	    
-		jugadorDos.setNombreJugador(j2nombre);
-		jugadorUno.setEdad(edadj2);
-	    jugadorUno.setIdioma(idiomaj2);
-		
-		
-	    assertAll("existencia",
-	            () -> assertEquals(null,jugadorUno.getNombreJugador()),
-	            () -> assertEquals(edadj2,jugadorDos.getEdad()),
-	            () -> assertEquals(idiomaj2,jugadorDos.getIdioma());
-		
-		/*fail("Not yet implemented");*/
+		AsignarEquipo asignarEquipo = new AsignarEquipo();
+
+        /*Creamos un jugador con todos los campos válidos*/
+        Jugador jugador = new Jugador();
+        String nombrevalido = "1234";
+        int edadValida = 18;
+        String idiomaValido = "Aleman";
+        
+        jugador.setNombreJugador(nombrevalido);
+        jugador.setEdad(edadValida);
+        jugador.setIdioma(idiomaValido);
+
+        /*Asignar el jugador creado en asignar equipo*/
+        asignarEquipo.setJugador(jugador);
+
+        Jugador jugadorGuardado = asignarEquipo.getJugador();
+       
+        assertNotNull(jugadorGuardado);
+        assertEquals(null, jugadorGuardado.getNombreJugador());
+        assertEquals(edadValida, jugadorGuardado.getEdad());
+        assertEquals(idiomaValido, jugadorGuardado.getIdioma());
 	}
 	
 	/**
@@ -186,38 +102,27 @@ class AsignarEquipoTest {
 	 */
 	@Test
 	void testSetNombreJugadorNombreVacio() {
-		Jugador jugadorUno = new Jugador();
-	    Jugador jugadorDos = new Jugador();
-	    
-		String j1nombre = "MariaMariaMariaMaria";
-		String j2nombre = " ";
-		
-		int edadj1 = 31;
-	    int edadj2 = 30;
-	    
-	    String idiomaj1 = "Español";
-	    String idiomaj2 = "Español";
-	    
-	    String idioma1 = "Ingles";
-	    String idioma2 = "Frances";
-	    String idioma3 = "Aleman";
-	    String idioma4 = "Español";
-	    
-		jugadorUno.setNombreJugador(j1nombre);
-		jugadorUno.setEdad(edadj1);
-	    jugadorUno.setIdioma(idiomaj1);
-	    
-		jugadorDos.setNombreJugador(j2nombre);
-		jugadorUno.setEdad(edadj2);
-	    jugadorUno.setIdioma(idiomaj2);
-		
-		
-	    assertAll("existencia",
-	            () -> assertEquals(null,jugadorUno.getNombreJugador()),
-	            () -> assertEquals(edadj2,jugadorDos.getEdad()),
-	            () -> assertEquals(idiomaj2,jugadorDos.getIdioma());
-		
-		/*fail("Not yet implemented");*/
+		AsignarEquipo asignarEquipo = new AsignarEquipo();
+
+        /*Creamos un jugador con todos los campos válidos*/
+        Jugador jugador = new Jugador();
+        String nombrevalido = "";
+        int edadValida = 99;
+        String idiomaValido = "Ingles";
+        
+        jugador.setNombreJugador(nombrevalido);
+        jugador.setEdad(edadValida);
+        jugador.setIdioma(idiomaValido);
+
+        /*Asignar el jugador creado en asignar equipo*/
+        asignarEquipo.setJugador(jugador);
+
+        Jugador jugadorGuardado = asignarEquipo.getJugador();
+       
+        assertNotNull(jugadorGuardado);
+        assertEquals(null, jugadorGuardado.getNombreJugador());
+        assertEquals(edadValida, jugadorGuardado.getEdad());
+        assertEquals(idiomaValido, jugadorGuardado.getIdioma());
 	}
 	
 	/**
@@ -225,38 +130,27 @@ class AsignarEquipoTest {
 	 */
 	@Test
 	void testSetNombreJugadorPruebaEdadNoValida() {
-		Jugador jugadorUno = new Jugador();
-	    Jugador jugadorDos = new Jugador();
-	    
-		String j1nombre = "MariaMariaMariaMaria";
-		String j2nombre = "MariaMariaMaria";
-		
-		int edadj1 = 31;
-	    int edadj2 = 'a';
-	    
-	    String idiomaj1 = "Español";
-	    String idiomaj2 = "Español";
-	    
-	    String idioma1 = "Ingles";
-	    String idioma2 = "Frances";
-	    String idioma3 = "Aleman";
-	    String idioma4 = "Español";
-	    
-		jugadorUno.setNombreJugador(j1nombre);
-		jugadorUno.setEdad(edadj1);
-	    jugadorUno.setIdioma(idiomaj1);
-	    
-		jugadorDos.setNombreJugador(j2nombre);
-		jugadorUno.setEdad(edadj2);
-	    jugadorUno.setIdioma(idiomaj2);
-		
-		
-	    assertAll("existencia",
-	            () -> assertEquals(j2nombre,jugadorDos.getNombreJugador()),
-	            () -> assertEquals(null,jugadorDos.getEdad()),
-	            () -> assertEquals(idiomaj2,jugadorDos.getIdioma());
-		
-		/*fail("Not yet implemented");*/
+		AsignarEquipo asignarEquipo = new AsignarEquipo();
+
+        /*Creamos un jugador con todos los campos válidos*/
+        Jugador jugador = new Jugador();
+        String nombrevalido = "Maria";
+        int edadValida = 15;
+        String idiomaValido = "Español";
+        
+        jugador.setNombreJugador(nombrevalido);
+        jugador.setEdad(edadValida);
+        jugador.setIdioma(idiomaValido);
+
+        /*Asignar el jugador creado en asignar equipo*/
+        asignarEquipo.setJugador(jugador);
+
+        Jugador jugadorGuardado = asignarEquipo.getJugador();
+       
+        assertNotNull(jugadorGuardado);
+        assertEquals(nombrevalido, jugadorGuardado.getNombreJugador());
+        assertEquals(0, jugadorGuardado.getEdad());
+        assertEquals(idiomaValido, jugadorGuardado.getIdioma());
 	}
 	
 	/**
@@ -264,38 +158,27 @@ class AsignarEquipoTest {
 	 */
 	@Test
 	void testSetNombreJugadorPruebaNoHayEdad() {
-		Jugador jugadorUno = new Jugador();
-	    Jugador jugadorDos = new Jugador();
-	    
-		String j1nombre = "MariaMariaMariaMaria";
-		String j2nombre = "MariaMariaMaria";
-		
-		int edadj1 = 31;
-	    int edadj2 ;
-	    
-	    String idiomaj1 = "Español";
-	    String idiomaj2 = "Español";
-	    
-	    String idioma1 = "Ingles";
-	    String idioma2 = "Frances";
-	    String idioma3 = "Aleman";
-	    String idioma4 = "Español";
-	    
-		jugadorUno.setNombreJugador(j1nombre);
-		jugadorUno.setEdad(edadj1);
-	    jugadorUno.setIdioma(idiomaj1);
-	    
-		jugadorDos.setNombreJugador(j2nombre);
-		jugadorUno.setEdad(edadj2);
-	    jugadorUno.setIdioma(idiomaj2);
-		
-		
-	    assertAll("existencia",
-	            () -> assertEquals(j2nombre,jugadorDos.getNombreJugador()),
-	            () -> assertEquals(null,jugadorUno.getEdad() != jugadorDos.getEdad()),
-	            () -> assertEquals(idiomaj2,jugadorDos.getIdioma());
-		
-		/*fail("Not yet implemented");*/
+		AsignarEquipo asignarEquipo = new AsignarEquipo();
+
+        /*Creamos un jugador con todos los campos válidos*/
+        Jugador jugador = new Jugador();
+        String nombrevalido = "Artsiom";
+        int edadValida = 0;
+        String idiomaValido = "Aleman";
+        
+        jugador.setNombreJugador(nombrevalido);
+        jugador.setEdad(edadValida);
+        jugador.setIdioma(idiomaValido);
+
+        /*Asignar el jugador creado en asignar equipo*/
+        asignarEquipo.setJugador(jugador);
+
+        Jugador jugadorGuardado = asignarEquipo.getJugador();
+       
+        assertNotNull(jugadorGuardado);
+        assertEquals(nombrevalido, jugadorGuardado.getNombreJugador());
+        assertEquals(0, jugadorGuardado.getEdad());
+        assertEquals(idiomaValido, jugadorGuardado.getIdioma());
 	}
 	
 	/**
@@ -303,37 +186,27 @@ class AsignarEquipoTest {
 	 */
 	@Test
 	void testSetNombreJugadorPruebaIdiomaNoValido() {
-		Jugador jugadorUno = new Jugador();
-	    Jugador jugadorDos = new Jugador();
-	    
-		String j1nombre = "MariaMariaMariaMaria";
-		String j2nombre = "MariaMariaMaria";
-		
-		int edadj1 = 31;
-	    int edadj2 = 30;
-	    
-	    String idiomaj1 = "Español";
-	    String idiomaj2 = "Japones";
-	    
-	    String idioma1 = "Ingles";
-	    String idioma2 = "Frances";
-	    String idioma3 = "Aleman";
-	    String idioma4 = "Español";
-	    
-		jugadorUno.setNombreJugador(j1nombre);
-		jugadorUno.setEdad(edadj1);
-	    jugadorUno.setIdioma(idiomaj1);
-		jugadorDos.setNombreJugador(j2nombre);
-		jugadorUno.setEdad(edadj2);
-	    jugadorUno.setIdioma(idiomaj2);
-		
-		
-	    assertAll("existencia",
-	            () -> assertEquals(j2nombre,jugadorDos.getNombreJugador()),
-	            () -> assertEquals(edadj2,jugadorDos.getEdad()),
-	            () -> assertEquals(null,jugadorDos.getIdioma());
-		
-		/*fail("Not yet implemented");*/
+		AsignarEquipo asignarEquipo = new AsignarEquipo();
+
+        /*Creamos un jugador con todos los campos válidos*/
+        Jugador jugador = new Jugador();
+        String nombrevalido = "Artsiom";
+        int edadValida = 50;
+        String idiomaValido = "Japones";
+        
+        jugador.setNombreJugador(nombrevalido);
+        jugador.setEdad(edadValida);
+        jugador.setIdioma(idiomaValido);
+
+        /*Asignar el jugador creado en asignar equipo*/
+        asignarEquipo.setJugador(jugador);
+
+        Jugador jugadorGuardado = asignarEquipo.getJugador();
+       
+        assertNotNull(jugadorGuardado);
+        assertEquals(nombrevalido, jugadorGuardado.getNombreJugador());
+        assertEquals(edadValida, jugadorGuardado.getEdad());
+        assertEquals(null, jugadorGuardado.getIdioma());
 	}
 	
 	/**
@@ -341,37 +214,26 @@ class AsignarEquipoTest {
 	 */
 	@Test
 	void testSetNombreJugadorPruebaNoHayIdioma() {
-		Jugador jugadorUno = new Jugador();
-	    Jugador jugadorDos = new Jugador();
-	    
-		String j1nombre = "MariaMariaMariaMaria";
-		String j2nombre = "MariaMariaMaria";
-		
-		int edadj1 = 31;
-	    int edadj2 = 30;
-	    
-	    String idiomaj1 = "Español";
-	    String idiomaj2 = " ";
-	    
-	    String idioma1 = "Ingles";
-	    String idioma2 = "Frances";
-	    String idioma3 = "Aleman";
-	    String idioma4 = "Español";
-	    
-		jugadorUno.setNombreJugador(j1nombre);
-		jugadorUno.setEdad(edadj1);
-	    jugadorUno.setIdioma(idiomaj1);
-	    
-		jugadorDos.setNombreJugador(j2nombre);
-		jugadorUno.setEdad(edadj2);
-	    jugadorUno.setIdioma(idiomaj2);
-		
-		
-	    assertAll("existencia",
-	            () -> assertEquals(j2nombre,jugadorDos.getNombreJugador()),
-	            () -> assertEquals(edadj2,jugadorDos.getEdad()),
-	            () -> assertEquals(null,jugadorDos.getIdioma());
-		
-		/*fail("Not yet implemented");*/
+		AsignarEquipo asignarEquipo = new AsignarEquipo();
+
+        /*Creamos un jugador con todos los campos válidos*/
+        Jugador jugador = new Jugador();
+        String nombrevalido = "Antonio";
+        int edadValida = 19;
+        String idiomaValido = "";
+        
+        jugador.setNombreJugador(nombrevalido);
+        jugador.setEdad(edadValida);
+        jugador.setIdioma(idiomaValido);
+
+        /*Asignar el jugador creado en asignar equipo*/
+        asignarEquipo.setJugador(jugador);
+
+        Jugador jugadorGuardado = asignarEquipo.getJugador();
+       
+        assertNotNull(jugadorGuardado);
+        assertEquals(nombrevalido, jugadorGuardado.getNombreJugador());
+        assertEquals(edadValida, jugadorGuardado.getEdad());
+        assertEquals(idiomaValido, jugadorGuardado.getIdioma());
 	}
 }
