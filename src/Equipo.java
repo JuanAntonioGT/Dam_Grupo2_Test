07529bce0c9a1ea606200eaad47cc11001f496fd
Álvaro Martín Bazán <93922404+AlvaroBazan@users.noreply.github.com>
2022-02-04 14:51:23 +0100

    Alvaro equipo 4/2/2022
    c

--- src/Equipo.java
+++ src/Equipo.java
@@ -1,4 +1,3 @@
-
 public class Equipo {
 	private String nombreEquipo;
 	private int ranking;
@@ -15,7 +14,14 @@ public class Equipo {
 	}
 
 	public void setNombreEquipo(String nombreEquipo) {
-		if (nombreEquipo.length() > 4 && nombreEquipo.length() < 20) {
+
+		if (nombreEquipo.length() >= 4 && nombreEquipo.length() <= 20) {
+			for (int i = 0; i < nombreEquipo.length(); i++) {
+				if(i==1 || i==2 || i==3|| i==4|| i==5|| i==6|| i==7|| i==8|| i==9|| i==0){
+					this.nombreEquipo=null;
+				}
+
+			}
 			this.nombreEquipo = nombreEquipo;
 		}
 	}
@@ -26,17 +32,26 @@ public class Equipo {
 	}
 
 	public void setRanking(int ranking) {
-		if (ranking > 0){
-			this.ranking = ranking;
+		for (int i = 0; i < ranking.length(); i++) {
+			
 		}
+		if(ranking%1 == 0) {
 
+			if (ranking >= 1 && ranking <= 10) {
+				System.out.println("Ranking guardado");
+				this.ranking = ranking;
+			}
 
+		}
 	}
+
+
+
 	// Creado por alvaro martin bazan
 	public String getCategoriaEquipo() {
-		String tercera = "Tercera Categoría";
-		String segunda= "Segunda Categoría";
-		String primera= "Primera Categoría";
+		String tercera = "Tercera";
+		String segunda= "Segunda";
+		String primera= "Primera";
 		if (this.nombreEquipo != null){
 			if(this.ranking<3 && this.ranking>=1){
 
@@ -45,6 +60,8 @@ public class Equipo {
 				return segunda;
 			}else if(this.ranking>=7 && this.ranking <=10){
 				return primera;
+			}else{
+				return null;
 			}
 		}
 		return categoriaEquipo;
@@ -54,3 +71,4 @@ public class Equipo {
 		this.categoriaEquipo = categoriaEquipo;
 	}
 }
+
