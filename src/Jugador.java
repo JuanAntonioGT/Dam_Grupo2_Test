7fba3981581dff3903a3b8e91e2eae60cd381659
JuanAntonioGT <93922490+JuanAntonioGT@users.noreply.github.com>
2022-02-11 14:54:27 +0100

    .
    .

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,103 +1,170 @@
 public class Jugador {
-    private String nombre;
-    private int edad;
-    private String idioma;
-    private String tipo;
-    
-    //story 5
-     private String tipo;
-    //
-    public Jugador (String nombre, int edad, String idioma) {
-        this.setNombreJugador(nombre);
-        this.setEdad(edad);
-        this.setIdioma(idioma);
+	private String nombre;
+	private int edad;
+	private String idioma;
+
+	/*NOMBRE*/
+	public void setNombreJugador(String nombre) {
+		if (isValidated(nombre)){
+
+			//Pasamos a mayusculas para facilitar el trabajo
+			String nombreMayus = nombre.toUpperCase();
+
+			//Comprobamos el Rango
+			if (isInRange(nombreMayus)) {
+
+				//Comprobamos que sea del abecedario español
+				if (isWord(nombreMayus)){
+					this.nombre = nombreMayus;
+				}
+			}
+		}
+	}
+
+	public String getNombreJugador() {
+		return nombre;
+	}
+
+	/*EDAD*/
+	public void setEdad(int edad) {
+
+		//Comprobamos el Rango de edad
+		if (isInYear(edad)){
+			this.edad = edad;
+		}
+
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	/*IDIOMA*/
+	public void setIdioma(String idioma) {
+		// A IDIOMA NO PUEDE LLEGAR MAS DE 1 IDIOMA
+
+		//Pasamos el idioma a minusculas
+		String idiomaMin = idioma.toLowerCase();
+
+		//Revisamos que no sea nulo y no este vacio
+		if (isValidated(idiomaMin)) {
+			if (isInLanguage(idiomaMin)) {
+				this.idioma = idioma;
+			}
+
+		}
+	}
+
+	public String getIdioma() {
+		return idioma;
+	}
 
 
-    }
+	//Story 5
+	/*TIPO*/
+	public String getTipoJugador() {
+		//String para los rangos de clases
+		String junior = "Junior";
+		String senior = "Senior";
+		String master = "Master";
 
-    public void setNombreJugador(String nombre) {
-        //Pasamos a mayusculas para facilitar el trabajo
-        String nombreMayus = nombre.toUpperCase();
+		//Int para rangos de edades
+		int edadMinJunior = 18;
+		int edadMaxJunior = 25;
+		int edadMinSenior = 26;
+		int edadMaxSenior = 35;
+		int edadMinMaster = 36;
+		int edadCero = 0;
 
-        //Revisamos que no sea nulo
-        if (nombreMayus != null){
-             //Revisamos el largo del nombre
-             if (nombre.length()>=4 && nombre.length()<=20 ){
-                //Recorremos el nombre por su largo
-                for (int i = 0; i < nombre.length(); i++) {
-                    //En cada vuelta revisamos las letras
-                    char letra = nombreMayus.charAt(i);
-                    //Comprobamos que esten entre A y Z
-                    if (letra >= 65 && letra <= 90 || letra == 32) {
-                        this.nombre=nombre.toUpperCase();
-                    }
-                }
-            }
-        }
+			if (this.edad >= edadMinJunior
+					&& this.edad <= edadMaxJunior) {
 
+				return junior;
 
-    }
+			} else if (this.edad >= edadMinSenior
+					&& this.edad <= edadMaxSenior) {
 
-    public void setEdad(int edad) {
+				return senior;
 
+			} else if (this.edad >= edadMinMaster) {
 
-                if (edad != 0){
-                    if (edad >= 18) {
-                        if (edad <= 100) {
-                            this.edad=edad;
-                        }
-                    }
-                }
-            
+				return master;
 
-    }
-    public void setIdioma(String idioma) {
-        String aleman = "aleman";
-        String ingles = "ingles";
-        String espanol = "español";
-        String frances = "frances";
-        String idiomaMin = idioma.toLowerCase();
+			} else {
+				return null;
+			}
 
-        if(idiomaMin != null){
-            if(idiomaMin != " "){
-                if(idiomaMin.equals(aleman) || idiomaMin.equals(ingles) || idiomaMin.equals(espanol) || idiomaMin.equals(frances) ){
-                    this.idioma=idioma;
-                }
-            }
-        }
-    }
+	}
 
+	//Nos devuelve un true si es distinto a nulo.
+	private Boolean isValidated(String comprobar) {
 
-    public String getIdioma() {
-        return idioma;
-    }
+		if (comprobar != null) {
 
-    public String getNombreJugador() {
-        return nombre;
-    }
-    public int getEdad() {
-        return edad;
-    }
-    
-    //Story 5
-    public void setTipo(String tipo) {
-        if(edad >=18 && edad <=25){
-            this.tipo = "Junior";
-            if(edad >=26 && edad <=35) {
-                this.tipo = "Senior";
-                if(edad >36) {
-                    this.tipo = "Master";
-                }
-            }
+			return true;
+		}
+		return false;
+	}
 
-          }
-       }
-    
-    public String getTipo() {
-        return tipo;
-    }
-    //
-    
-    
-     
+	//Nos devuelve un true si está en rango la longitud de la palabra
+	private boolean isInRange(String comprobar){
+		int largoMin = 4;
+		int largoMax = 20;
+
+		if (comprobar.length() >= largoMin
+				&& comprobar.length() <= largoMax ){
+			return true;
+		}
+		return false;
+	}
+
+	//Nos devuelve un true si una letra del abecedario español
+	private boolean isWord(String comprobar){
+		char a = 65;
+		char z = 90;
+		char espacio = 32;
+
+		for (int i = 0; i < comprobar.length(); i++) {
+			char c = comprobar.charAt(i);
+			if (!(c >= a && c <= z || c==espacio)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	//Nos devuelve true si está en el rango de edad
+	private boolean isInYear(int comprobarEdad){
+		int edadCero = 0;
+		int edadMinima = 18;
+		int edadMaxima = 100;
+
+		if (comprobarEdad != edadCero){
+
+			if (comprobarEdad >= edadMinima && comprobarEdad <= edadMaxima){
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	//Nos devuelve true si el idioma es válido
+	private boolean isInLanguage(String idioma){
+		String[] listaDeIdiomas =  {"aleman", "ingles", "espanol", "frances"};
+		String vacio = " ";
+
+		if (!idioma.equals(vacio)){
+			for (int i = 0; i < listaDeIdiomas.length; i++) {
+				if (idioma.contains(listaDeIdiomas[i])){
+					return true;
+				}
+			}
+
+		}
+		return false;
+	}
+
 }
