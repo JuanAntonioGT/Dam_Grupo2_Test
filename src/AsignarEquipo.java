e55eb05029405a0377ad6a0038f6e7db50b76002
JuanAntonioGT <93922490+JuanAntonioGT@users.noreply.github.com>
2022-02-11 20:55:19 +0100

    Object QUITADO
    Lo que te advertía era porque yo usaba un método un tanto raruno. Ahora todo perfecto.

--- src/AsignarEquipo.java
+++ src/AsignarEquipo.java
@@ -2,8 +2,41 @@ public class AsignarEquipo {
       private Equipo equipo;
       private Jugador jugador;
 
+      public void setEquipo(Equipo equipo) {
+            if (isEquipoValidated(equipo)){
+                  if (hasString(equipo.getNombreEquipo())){
+                        if (hasInt(equipo.getRanking())){
+                              this.equipo = equipo;
+                        }
+                  }
+            }
+      }
+
+      public Equipo getEquipo(){
+            return this.equipo;
+      }
+
+      public void setJugador(Jugador jugador) {
+            //Comprobamos si viene null
+            if (isJugadorValidated(jugador)){
+                  if (hasString(jugador.getNombreJugador())){
+                        if (hasInt(jugador.getEdad())){
+                              if (hasString(jugador.getIdioma())) {
+                                    this.jugador = jugador;
+                              }
+                        }
+                  }
+            }
+      }
+
+
+
+      public Jugador getJugador() {
+            return this.jugador;
+      }
+
       //Nos devuelve un true si es distinto a nulo.
-      private Boolean isValidated(Object comprobar) {
+      private Boolean isJugadorValidated(Jugador comprobar) {
             boolean validated = true;
 
             if (comprobar == null) {
@@ -15,17 +48,19 @@ public class AsignarEquipo {
             return validated;
       }
 
-      public void setEquipo(Equipo equipo) {
-            if (isValidated(equipo)){
-                  if (hasName(equipo.getNombreEquipo())){
-                        if (hasRanking(equipo.getRanking())){
-                              this.equipo = equipo;
-                        }
-                  }
+      private Boolean isEquipoValidated(Equipo comprobar) {
+            boolean validated = true;
+
+            if (comprobar == null) {
+                  validated = false;
+            } else {
+                  validated = true;
             }
+
+            return validated;
       }
 
-      private boolean hasRanking(int rank){
+      private boolean hasInt(int rank){
             boolean hasRanking = true;
 
             if (rank != 0){
@@ -37,7 +72,7 @@ public class AsignarEquipo {
             return hasRanking;
       }
 
-      private boolean hasName(String name){
+      private boolean hasString(String name){
             boolean hasName = true;
 
             if (name != null){
@@ -50,28 +85,6 @@ public class AsignarEquipo {
                   hasName = false;
             }
             return hasName;
-      }
-
-
-      public Equipo getEquipo(){
-            return this.equipo;
-      }
-
-      public void setJugador(Jugador jugador) {
-            //Comprobamos si viene null
-            if (isValidated(jugador)) {
-                  if (isValidated(jugador.getNombreJugador())){
-                        if (isValidated(jugador.getIdioma())){
-                              if (jugador.getEdad() != 0){
-                                    this.jugador = jugador;
-                              }
-                        }
-                  }
-            }
-      }
-
-      public Jugador getJugador() {
-            return this.jugador;
       }
 
 }
